0d28fdc392058b801c419dc5e7669892f2302bfc
Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>
2018-10-31 21:30:51 +0100

    Add watcher's metrics
    Change-Id: I0d035db8c065da0c457ef66547ce1f194f1b6fc2
    Signed-off-by: Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>

---
+++ watcher/metrics.go
@@ -0,0 +1,114 @@
+/*
+ *  Copyright (c) 2018 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+
+package watcher
+
+import (
+	"github.com/SamsungSLAV/perun/metrics"
+	"github.com/SamsungSLAV/slav/logger"
+)
+
+const (
+	namespace = "perun"
+	submodule = "watcher"
+	// metric names:
+	workerIdle  = "worker_idle"
+	workerBusy  = "worker_busy"
+	queuedTasks = "queued_tasks"
+	queueSize   = "queue_size"
+)
+
+// gauges structure aggregates all gauges metrics used in watcher module for easy access in code.
+type gauges struct {
+	// metrics related to running tasks.
+	listSnapshots    metrics.Gauge
+	listPrereleases  metrics.Gauge
+	listRepositories metrics.Gauge
+	listImages       metrics.Gauge
+	listFiles        metrics.Gauge
+	getFileInfo      metrics.Gauge
+
+	// metrics related to workers' state.
+	workerBusy metrics.Gauge
+	workerIdle metrics.Gauge
+
+	// metrics related to pending tasks queue.
+	queueTasks metrics.Gauge
+	queueSize  metrics.Gauge
+}
+
+// registerMetric registers a single gauge metric with given name and help.
+// It also sets initial value of gauge to 0.
+func registerMetric(metrics metrics.Metrics, name, help string) (metrics.Gauge, error) {
+	g, err := metrics.RegisterGauge(namespace, submodule, name, help)
+	if err != nil {
+		logger.WithError(err).Error("Error registering Gauge.")
+		return nil, err
+	}
+	g.Set(0.0)
+	return g, nil
+}
+
+// registerMetrics registers all perun watcher's metrics.
+func (gauges *gauges) registerMetrics(metrics metrics.Metrics) error {
+	var err error
+	name := func(t TaskType) string { return "current_" + t }
+	help := func(t TaskType) string { return "Number of " + t + " running tasks." }
+
+	gauges.listSnapshots, err = registerMetric(metrics, name(ListSnapshots), help(ListSnapshots))
+	if err != nil {
+		return err
+	}
+	gauges.listPrereleases, err = registerMetric(metrics, name(ListPrereleases),
+		help(ListPrereleases))
+	if err != nil {
+		return err
+	}
+	gauges.listRepositories, err = registerMetric(metrics, name(ListRepositories),
+		help(ListRepositories))
+	if err != nil {
+		return err
+	}
+	gauges.listImages, err = registerMetric(metrics, name(ListImages), help(ListImages))
+	if err != nil {
+		return err
+	}
+	gauges.listFiles, err = registerMetric(metrics, name(ListFiles), help(ListFiles))
+	if err != nil {
+		return err
+	}
+	gauges.getFileInfo, err = registerMetric(metrics, name(GetFileInfo), help(GetFileInfo))
+	if err != nil {
+		return err
+	}
+
+	gauges.workerBusy, err = registerMetric(metrics, workerBusy, "Number of busy workers.")
+	if err != nil {
+		return err
+	}
+	gauges.workerIdle, err = registerMetric(metrics, workerIdle, "Number of idle workers.")
+	if err != nil {
+		return err
+	}
+
+	gauges.queueTasks, err = registerMetric(metrics, queuedTasks,
+		"Number of tasks pending in queue.")
+	if err != nil {
+		return err
+	}
+	gauges.queueSize, err = registerMetric(metrics, queueSize, "Size of the task queue.")
+	return err
+}
